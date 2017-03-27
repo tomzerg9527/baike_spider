a6dd05fc95b5fbf1a36cedbe8b9719b5a550f1a1
陆健 <mcc_cool1257@hotmail.com>
2017-03-27 20:46:00 +0800

    Add files via upload

---
+++ baike_spider/html_downloader.py
@@ -0,0 +1,31 @@
+'''
+
+
+@author: Administrator
+'''
+import urllib2
+
+
+
+
+
+
+class HtmlDownloader(object):
+    
+    
+    def download(self,url):
+        if url is None:
+            return None
+        
+        response = urllib2.urlopen(url)
+        
+        if response.getcode() != 200:
+            return None
+        
+        return response.read()
+         
+    
+    
+
+
+
